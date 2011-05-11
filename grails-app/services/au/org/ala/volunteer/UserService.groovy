35b38721891e40049802500f6f8c99a7059ce82e
djtfmartin <djtfmartin@gmail.com>
2011-05-11 10:45:45 +0000

    various changes including user list
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk/volunteer-portal@41 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- grails-app/services/au/org/ala/volunteer/UserService.groovy
+++ grails-app/services/au/org/ala/volunteer/UserService.groovy
@@ -10,11 +10,15 @@ class UserService {
 
     def registerCurrentUser = {
       def userId = authService.username()
+      def displayName = authService.displayName()
+      println("Checking user is registered: " + userId+", " + displayName )
+
       if(userId){
         if(User.findByUserId(userId)==null){
           User user = new User()
           user.userId = userId
           user.created = new Date()
+          user.displayName = displayName
           user.save(flush:true)
         }
       }
