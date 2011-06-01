34dbdcdaf5f0b29e1a12409bad2269d579e0e2db
djtfmartin <djtfmartin@gmail.com>
2011-06-01 06:20:57 +0000

    auth fix
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk/volunteer-portal@90 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- grails-app/controllers/au/org/ala/volunteer/UserController.groovy
+++ grails-app/controllers/au/org/ala/volunteer/UserController.groovy
@@ -43,6 +43,7 @@ class UserController {
 
     def show = {
         def userInstance = User.get(params.id)
+
         if (!userInstance) {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
             redirect(action: "list")
