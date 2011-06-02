e37baf757191e20a7d47efa9d01a944d42080df8
djtfmartin <djtfmartin@gmail.com>
2011-06-02 00:10:42 +0000

    fixes and temporary styling
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk@98 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- volunteer-portal/grails-app/controllers/au/org/ala/volunteer/TaskController.groovy
+++ volunteer-portal/grails-app/controllers/au/org/ala/volunteer/TaskController.groovy
@@ -29,7 +29,7 @@ class TaskController {
 
     /** list all tasks */
     def list = {
-        params.max = Math.min(params.max ? params.int('max') : 50, 100)
+        params.max = Math.min(params.max ? params.int('max') : 10, 20)
         [taskInstanceList: Task.list(params), taskInstanceTotal: Task.count()]
     }
 
