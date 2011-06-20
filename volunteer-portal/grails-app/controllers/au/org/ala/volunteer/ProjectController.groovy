6b5f5530c3ef6c9cd9665bda247238cab3d06d29
nickdos <nickdos@gmail.com>
2011-06-20 04:55:03 +0000

    Added transcribedBy to map popup on project home page.
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk@144 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- volunteer-portal/grails-app/controllers/au/org/ala/volunteer/ProjectController.groovy
+++ volunteer-portal/grails-app/controllers/au/org/ala/volunteer/ProjectController.groovy
@@ -28,8 +28,9 @@ class ProjectController {
             def allUsersMap = [:]
             taskList.each {
                 Map recordValues = fieldSyncService.retrieveFieldsForTask(it)
-                taskListFields.add(recordValues)
                 def userId = it.fullyTranscribedBy
+                recordValues.get(0).transcribedBy = User.findByUserId(userId?:"")?.displayName
+                taskListFields.add(recordValues)
                 if (userId) {
                     def count = (allUsersMap.containsKey(userId)) ? (allUsersMap.get(userId) + 1) : 1
                     allUsersMap.put(userId , count)
