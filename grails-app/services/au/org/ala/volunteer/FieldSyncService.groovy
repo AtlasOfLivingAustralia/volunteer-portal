654dd8b5cf80021f6bfd8bdd512d6d565b588c7b
nickdos <nickdos@gmail.com>
2011-09-02 02:57:05 +0000

    Fixed bug where changes by another user were not being displayed.
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk/volunteer-portal@195 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- grails-app/services/au/org/ala/volunteer/FieldSyncService.groovy
+++ grails-app/services/au/org/ala/volunteer/FieldSyncService.groovy
@@ -4,131 +4,135 @@ import org.springframework.validation.Errors
 
 class FieldSyncService {
 
-  static transactional = true
-  def serviceMethod() {}
+    static transactional = true
 
-  Map retrieveFieldsForTask(Task taskInstance){
-    Map recordValues = new LinkedHashMap()
-    taskInstance.fields.each { field ->
-      def recordMap = recordValues.get(field.recordIdx)
-      if(recordMap==null){
-        recordMap = new LinkedHashMap()
-        recordValues.put field.recordIdx, recordMap
-      }
-      recordMap.put field.name,field.value
-    }
-    log.debug("record values: " + recordValues )
-    recordValues
-  }
+    def serviceMethod() {}
 
-  /**
-   * Takes some new field values and sensibly syncs with existing field values
-   * in the database.
-   *
-   * @param record
-   * @param fieldValues
-   * @return
-   */
-  void syncFields(Task task, Map fieldValues, String transcriberUserId, boolean markAsFullyTranscribed, boolean markAsFullyValidated) {
-
-    //sync
-    def idx = 0
-    def hasMore = true
-    while (hasMore) {
-      def fieldValuesForRecord = fieldValues.get(idx.toString())
-      if (fieldValuesForRecord) {
-
-        //get existing fields, and add to a map
-        def oldFields = Field.executeQuery("from Field f where task = :task and recordIdx = :recordIdx and superceded = false",
-                [task: task, recordIdx: idx])
-
-        Map oldFieldValues = new LinkedHashMap()
-        oldFields.each { field -> oldFieldValues.put(field.name, field) }
-
-        fieldValuesForRecord.each { keyValue ->
-
-          Field oldFieldValue = oldFieldValues.get(keyValue.key)
-          if (oldFieldValue != null) {
-
-            if (oldFieldValue.value != keyValue.value) {
-              //if different users
-              if (oldFieldValue.transcribedByUserId != transcriberUserId) {
-                //just save it
-                Field field = new Field()
-                field.name = keyValue.key
-                field.value = keyValue.value
-                field.transcribedByUserId = transcriberUserId
-                field.task = task
-                field.updated = new Date()
-                field.save(flush: true)
-                if(field.hasErrors()) {
-                  field.errors.allErrors.each { log.error(it) }
-                  task.errors.reject(field.errors.toString())
-                  return;
-                }
-
-                //keep the original, but mark as superceded
-                oldFieldValue.superceded = true
-                oldFieldValue.updated = new Date()
-                oldFieldValue.save(flush: true)
-                if(oldFieldValue.hasErrors()) {
-                  oldFieldValue.errors.allErrors.each { log.error(it) }
-                  task.errors.reject(oldFieldValue.errors.toString())
-                  //task.errors.addAllErrors(oldFieldValue.errors)
-                  return;
-                }
-
-              } else {
-                //just replace the value
-                oldFieldValue.value = keyValue.value
-                oldFieldValue.updated = new Date()
-                oldFieldValue.save(flush: true)
-
-                if(oldFieldValue.hasErrors()) {
-                  oldFieldValue.errors.allErrors.each { log.error(it) }
-                  task.errors.reject(oldFieldValue.errors.toString())
-                  //task.errors.addAllErrors(oldFieldValue.errors)
-                  return;
-                }
-              }
+    Map retrieveFieldsForTask(Task taskInstance) {
+        Map recordValues = new LinkedHashMap()
+        taskInstance.fields.each { field ->
+            def recordMap = recordValues.get(field.recordIdx)
+            if (recordMap == null) {
+                recordMap = new LinkedHashMap()
+                recordValues.put field.recordIdx, recordMap
             }
-
-          } else {
-            //persist these values
-            Field field = new Field(recordIdx: idx, name: keyValue.key, value: keyValue.value,
-                    task: task, transcribedByUserId: transcriberUserId, superceded: false)
-            field.save(flush: true)
-            if(field.hasErrors()) {
-               field.errors.allErrors.each { log.error(it) }
-               task.errors.reject(field.errors.toString())
-               return;
+            if (!field.superceded) {
+                recordMap.put field.name, field.value
             }
-          }
         }
-        idx = idx + 1
-      } else {
-        hasMore = false
-      }
+        log.debug("record values: " + recordValues)
+        recordValues
     }
 
-    //set the transcribed by
-    if(markAsFullyTranscribed){
-      task.fullyTranscribedBy = transcriberUserId
-      def user = User.findByUserId(transcriberUserId)
-      user.transcribedCount++
-      user.save(flush:true)
-    }
+    /**
+     * Takes some new field values and sensibly syncs with existing field values
+     * in the database.
+     *
+     * @param record
+     * @param fieldValues
+     * @return
+     */
+    void syncFields(Task task, Map fieldValues, String transcriberUserId, boolean markAsFullyTranscribed, boolean markAsFullyValidated) {
 
-    if(markAsFullyValidated){
-      task.fullyValidatedBy = transcriberUserId
-      def user = User.findByUserId(transcriberUserId)
-      user.validatedCount++
-      user.save(flush:true)
-    } else {
-      //reset the fully validated flag
-      task.fullyValidatedBy = null
-    }
+        //sync
+        def idx = 0
+        def hasMore = true
+        while (hasMore) {
+            def fieldValuesForRecord = fieldValues.get(idx.toString())
+            if (fieldValuesForRecord) {
 
-    task.save(flush:true)
-  }
+                //get existing fields, and add to a map
+                def oldFields = Field.executeQuery("from Field f where task = :task and recordIdx = :recordIdx and superceded = false",
+                        [task: task, recordIdx: idx])
+
+                Map oldFieldValues = new LinkedHashMap()
+                oldFields.each { field -> oldFieldValues.put(field.name, field) }
+
+                fieldValuesForRecord.each { keyValue ->
+
+                    Field oldFieldValue = oldFieldValues.get(keyValue.key)
+                    if (oldFieldValue != null) {
+
+                        if (oldFieldValue.value != keyValue.value) {
+                            //if different users
+                            if (oldFieldValue.transcribedByUserId != transcriberUserId) {
+                                //just save it
+                                Field field = new Field()
+                                field.name = keyValue.key
+                                field.value = keyValue.value
+                                field.transcribedByUserId = transcriberUserId
+                                field.task = task
+                                field.recordIdx = idx
+                                field.updated = new Date()
+                                field.save(flush: true)
+                                if (field.hasErrors()) {
+                                    field.errors.allErrors.each { log.error(it) }
+                                    task.errors.reject(field.errors.toString())
+                                    return;
+                                }
+
+                                //keep the original, but mark as superceded
+                                oldFieldValue.superceded = true
+                                oldFieldValue.updated = new Date()
+                                oldFieldValue.save(flush: true)
+                                if (oldFieldValue.hasErrors()) {
+                                    oldFieldValue.errors.allErrors.each { log.error(it) }
+                                    task.errors.reject(oldFieldValue.errors.toString())
+                                    //task.errors.addAllErrors(oldFieldValue.errors)
+                                    return;
+                                }
+
+                            } else {
+                                //just replace the value
+                                oldFieldValue.value = keyValue.value
+                                oldFieldValue.updated = new Date()
+                                oldFieldValue.save(flush: true)
+
+                                if (oldFieldValue.hasErrors()) {
+                                    oldFieldValue.errors.allErrors.each { log.error(it) }
+                                    task.errors.reject(oldFieldValue.errors.toString())
+                                    //task.errors.addAllErrors(oldFieldValue.errors)
+                                    return;
+                                }
+                            }
+                        }
+
+                    } else {
+                        //persist these values
+                        Field field = new Field(recordIdx: idx, name: keyValue.key, value: keyValue.value,
+                                task: task, transcribedByUserId: transcriberUserId, superceded: false)
+                        field.save(flush: true)
+                        if (field.hasErrors()) {
+                            field.errors.allErrors.each { log.error(it) }
+                            task.errors.reject(field.errors.toString())
+                            return;
+                        }
+                    }
+                }
+                idx = idx + 1
+            } else {
+                hasMore = false
+            }
+        }
+
+        //set the transcribed by
+        if (markAsFullyTranscribed) {
+            task.fullyTranscribedBy = transcriberUserId
+            def user = User.findByUserId(transcriberUserId)
+            user.transcribedCount++
+            user.save(flush: true)
+        }
+
+        if (markAsFullyValidated) {
+            task.fullyValidatedBy = transcriberUserId
+            def user = User.findByUserId(transcriberUserId)
+            user.validatedCount++
+            user.save(flush: true)
+        } else {
+            //reset the fully validated flag
+            task.fullyValidatedBy = null
+        }
+
+        task.save(flush: true)
+    }
 }
