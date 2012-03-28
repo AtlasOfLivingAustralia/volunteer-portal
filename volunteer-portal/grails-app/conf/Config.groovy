578b9c9e5a96111816497f4932897500767daeeb
nickdos <nickdos@gmail.com>
2012-03-28 00:56:25 +0000

    merged conf file.
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk@306 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- volunteer-portal/grails-app/conf/Config.groovy
+++ volunteer-portal/grails-app/conf/Config.groovy
@@ -136,9 +136,9 @@ environments {
         log4j.appender.'errors.File'="/var/log/tomcat/stacktrace.log"
     }
     development {
-        grails.serverURL = "http://baird.ala.org.au:8080/${appName}"
-        server.url = "http://baird.ala.org.au"
-        security.cas.appServerName = "http://baird.ala.org.au:8080"
+        grails.serverURL = "http://localhost:8080/${appName}"
+        server.url = "http://localhost"
+        security.cas.appServerName = "http://localhost:8080"
         security.cas.contextPath = "/${appName}"
         //log4j.appender.'errors.File'="stacktrace.log"
     }
@@ -148,7 +148,8 @@ environments {
         security.cas.appServerName = "http://bdrs-test.ala.org.au:8080"
         security.cas.contextPath = "/${appName}"
         log4j.appender.'errors.File'="/var/log/tomcat/stacktrace.log"
-    }}
+    }
+}
 
 //hibernate.SQL="trace,stdout"
 //hibernate.type="trace,stdout"
