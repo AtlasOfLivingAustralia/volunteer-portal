8dda10c5f5ba9eeae9512832579ccb93636cb937
sbearcsiro <simon.bear@csiro.au>
2014-06-13 08:10:55 +0000

    Add alias from /ajax/* to /ws/*
    git-svn-id: http://ala-volunteer.googlecode.com/svn/trunk/volunteer-portal@714 0b7a649d-3ec8-f03e-b044-56c4ea68b8d3

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,14 @@
 class UrlMappings {
 
 	static mappings = {
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
+
+        "/ws/$action?/$id?"(controller: 'ajax')
 
 		"/"(controller: "/index")
 		"500"(view:'/error')
