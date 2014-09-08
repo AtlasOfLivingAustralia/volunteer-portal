c5e74a836f5b29a9afbf1b134dc322ae0a354927
Bear, Simon (CSIRO IM&T, Yarralumla) <Simon.Bear@csiro.au>
2014-09-08 16:14:32 +1000

    Institution news
    Fixes #39 Link between Institution and Expedition gets broken when project is deactivated
    Closes #8 Institution level news items
    Add Expedition Institution migration page

---
+++ test/unit/au/org/ala/volunteer/FuzzyTest.groovy
@@ -0,0 +1,15 @@
+package au.org.ala.volunteer
+
+import org.junit.Test
+
+class FuzzyTest {
+
+    @Test
+    public void testLev() {
+        String s = "nihwearaweraaaoa"
+        String t = "bazxfgcfaaaingazsdgzdfg"
+        println(Fuzzy.levenshteinDistance(s,t))
+        println(Fuzzy.ldRatio(s,t))
+    }
+
+}
