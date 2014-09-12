522b70ecd59190460ffcb627cde79053405563df
davidbairdala <david.baird.ala@gmail.com>
2014-09-12 11:26:58 +1000

    Fixed issue with picklist loading empty strings instead of null for missing insitution codes

--- grails-app/services/au/org/ala/volunteer/PicklistService.groovy
+++ grails-app/services/au/org/ala/volunteer/PicklistService.groovy
@@ -56,7 +56,7 @@ class PicklistService {
                 if (m.find()) {
                     value = m.group(2);
                 }
-                def picklistItem = new PicklistItem(picklist: picklist, value: value, institutionCode: institutionCode)
+                def picklistItem = new PicklistItem(picklist: picklist, value: value, institutionCode: institutionCode ?: null)
 
                 if (tokens.size() > 1) {
                     picklistItem.key = tokens[1] // optional second value as "key"
