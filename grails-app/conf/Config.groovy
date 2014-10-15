a6feb8d4ac001b8184aa32e199a01c6739f557e0
Martin Bohun <martin.bohun@gmail.com>
2014-10-15 12:55:21 +1100

    adjusted grails.project.groupId
    removed DUPLICATE (originally it was a TRIPLICATE not a duplicate meaning groupId was set in this file 3 times, with the subsequent 2 overwriting the correctly set first occurance) and WRONG groupId (that was overwriting the correct groupId set earlier in this same file) in order to deploy into the correct package/dir hierarchy in the maven repository

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -147,7 +147,6 @@ bie.baseURL = "http://bie.ala.org.au"
 bie.searchPath = "/search"
 headerAndFooter.baseURL = "http://www2.ala.org.au/commonui"
 
-grails.project.groupId = "au.org.ala.volunteer" // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
