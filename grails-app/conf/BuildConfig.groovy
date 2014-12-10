e32945accd2dd8237205b9f63f08a04744c9f1e6
Angel Ruiz <aruizca@gmail.com>
2014-12-10 15:10:22 +1100

    Updates repository references to current ones

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,23 +20,14 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        mavenCentral()
-        mavenRepo "http://nexus.ala.org.au/content/repositories/releases/"
-        mavenRepo "http://maven.ala.org.au/repository/"
-        mavenRepo "http://snapshots.repository.codehaus.org"
-        mavenRepo "http://repository.codehaus.org"
-        mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "http://maven.tmatesoft.com/content/repositories/releases/"
+    repositories {
+        mavenLocal()
+        mavenRepo("http://nexus.ala.org.au/content/groups/public/") {
+            updatePolicy 'always'
+        }
     }
+
     plugins {
         build ":release:3.0.1"
         compile ":cache:1.1.8"
