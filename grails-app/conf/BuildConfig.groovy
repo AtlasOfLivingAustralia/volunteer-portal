36d77ce9ef2a9ea8ec0ac9e839d9a3df03d6b0d5
Angel Ruiz <aruizca@gmail.com>
2015-10-02 11:03:35 +1000

    Improvements added and clean up to BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,9 @@
+import grails.util.Environment
+
 //grails.project.class.dir = "target/classes"
 //grails.project.test.class.dir = "target/test-classes"
 //grails.project.test.reports.dir = "target/test-reports"
+grails.servlet.version = "3.0"
 grails.project.work.dir = "target"
 grails.project.war.file = "target/${appName}.war"
 
@@ -10,6 +13,7 @@ grails.project.fork = [
         war: false,
         console: false
 ]
+grails.reload.enabled = true
 
 grails.project.target.level = 1.7
 grails.project.source.level = 1.7
@@ -27,67 +31,9 @@ grails.project.dependency.resolution = {
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-
-        grailsPlugins()
-        grailsHome()
         mavenLocal()
         mavenRepo("http://nexus.ala.org.au/content/groups/public/") {
             updatePolicy 'always'
-        }
-        grailsCentral()
-        mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-
-    plugins {
-        build ":release:3.0.1"
-        compile ":cache:1.1.8"
-        runtime ":cache-ehcache:1.0.4"
-        build ':tomcat:7.0.55.3'
-//        runtime (':hibernate:3.6.10.18') { //runtime ":hibernate4:4.3.10"
-//            excludes 'net.sf.ehcache:ehcache-core'
-//        }
-        runtime ':hibernate4:4.3.10'
-
-        runtime ":jquery:1.11.1"
-        runtime ":jquery-ui:1.10.4"
-
-        // asset-pipeline 2.0+ requires Java 7, use version 1.9.x with Java 6
-        //compile ":asset-pipeline:2.5.1"
-
-        runtime ':resources:1.2.14'
-        //runtime ":lesscss-resources:1.3.3"
-        compile ":cache-headers:1.1.7"
-        runtime ":cached-resources:1.0"
-
-        runtime ":mail:1.0.7"
-        runtime ":csv:0.3.1"
-        runtime ":executor:0.3"
-        compile ":markdown:1.1.1"
-        runtime ":pretty-time:2.1.3.Final-1.0.1"
-        runtime ":quartz:1.0.2"
-        runtime ":tiny-mce:3.4.9"
-        runtime ":webxml:1.4.1"
-
-        //runtime ':ala-bootstrap3:1.3'
-        runtime ':twitter-bootstrap:3.3.5'
-        runtime ':font-awesome-resources:4.3.0.1'
-        runtime ':ala-auth:1.3.1'
-
-        compile ':scaffolding:2.1.2'
-        runtime ':database-migration:1.4.1'
-        compile (':webflow:2.1.0') {
-            excludes 'javassist'
-        }
-        compile ':build-info:1.2.8'
-        compile ":yammer-metrics:3.0.1-2"
-        //compile ":grails-melody:1.55.0"
-        compile(":images-client-plugin:0.3") {
-            excludes "ala-bootstrap2"
         }
     }
 
@@ -101,5 +47,48 @@ grails.project.dependency.resolution = {
         compile 'net.sf.opencsv:opencsv:2.3'
         compile 'org.freemarker:freemarker:2.3.23'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+    }
+
+    plugins {
+        build ":release:3.0.1"
+        compile ":cache:1.1.8"
+        runtime ":cache-ehcache:1.0.4"
+        build ':tomcat:7.0.55.3'
+        runtime ':hibernate4:4.3.10'
+
+        runtime ":jquery:1.11.1"
+        runtime ":jquery-ui:1.10.4"
+
+        runtime ':resources:1.2.14'
+        if (Environment.current == Environment.PRODUCTION) {
+            runtime ":zipped-resources:1.0.1"
+            runtime ":cached-resources:1.1"
+            compile ":cache-headers:1.1.7"
+            runtime ":yui-minify-resources:0.1.5"
+        }
+
+        runtime ":mail:1.0.7"
+        runtime ":csv:0.3.1"
+        runtime ":executor:0.3"
+        compile ":markdown:1.1.1"
+        runtime ":pretty-time:2.1.3.Final-1.0.1"
+        runtime ":quartz:1.0.2"
+        runtime ":tiny-mce:3.4.9"
+        runtime ":webxml:1.4.1"
+
+        runtime ':twitter-bootstrap:3.3.5'
+        runtime ':font-awesome-resources:4.3.0.1'
+        runtime ':ala-auth:1.3.1'
+
+        compile ':scaffolding:2.1.2'
+        runtime ':database-migration:1.4.1'
+        compile (':webflow:2.1.0') {
+            excludes 'javassist'
+        }
+        compile ':build-info:1.2.8'
+        compile ":yammer-metrics:3.0.1-2"
+        compile(":images-client-plugin:0.3") {
+            excludes "ala-bootstrap2"
+        }
     }
 }
