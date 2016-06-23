4667b2c4b02252c89bb7542b1ad780b487ddad3f
Simon Bear <simon.bear@csiro.au>
2016-06-23 23:46:01 +1000

    #236 image pop out broken

--- web-app/js/digivol-main.js
+++ web-app/js/digivol-main.js
@@ -1,29 +1,35 @@
 jQuery(function($) {
-  var unreadCount = 0;
-  if (BVP_JS_URLS && BVP_JS_URLS.unreadValidatedCount) {
-    $.getJSON(BVP_JS_URLS.unreadValidatedCount).done(function(data) {
-      unreadCount = data.count;
-      updateUnreadCount();
-    }).fail(function() {
-      console.warn("couldn't retrieve unread validated count", error);
+  try {
+    var unreadCount = 0;
+    if (BVP_JS_URLS && BVP_JS_URLS.unreadValidatedCount) {
+      $.getJSON(BVP_JS_URLS.unreadValidatedCount).done(function (data) {
+        unreadCount = data.count;
+        updateUnreadCount();
+      }).fail(function () {
+        console.warn("couldn't retrieve unread validated count", error);
+      });
+    } else {
+      console.warn("Unread validated count URL is not defined!");
+    }
+    function updateUnreadCount() {
+      if (unreadCount > 0) {
+        var text = unreadCount > 50 ? '50+' : unreadCount.toString();
+        $('.unread-count').text(text).removeClass('hidden');
+      } else {
+        $('.unread-count').addClass('hidden');
+      }
+    }
+
+    $(document).on('unreadValidationViewed', function (e, p) {
+      if (unreadCount > 0) {
+        unreadCount--;
+        updateUnreadCount();
+      } else {
+        console.warn("unread validation viewed when there are no unread validations to view?!");
+      }
     });
-  } else {
-    console.warn("Unread validated count URL is not defined!");
+  } catch (e) {
+    console.debug("couldn't get unread count", e);
+    // reference error, no BVP_JS_URLS
   }
-  function updateUnreadCount() {
-    if (unreadCount > 0) {
-      var text = unreadCount > 50 ? '50+' : unreadCount.toString();
-      $('.unread-count').text(text).removeClass('hidden');
-    } else {
-      $('.unread-count').addClass('hidden');
-    }
-  }
-  $(document).on('unreadValidationViewed', function(e, p) {
-    if (unreadCount > 0) {
-      unreadCount--;
-      updateUnreadCount();
-    } else {
-      console.warn("unread validation viewed when there are no unread validations to view?!");
-    }
-  });
 });
