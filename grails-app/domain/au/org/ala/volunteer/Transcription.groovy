0769e9ffb861982136a0d12774d06114e14b2f82
Patricia Koh <patricia.koh@csiro.au>
2018-10-25 11:38:31 +1100

    Add Transcription table

---
+++ grails-app/domain/au/org/ala/volunteer/Transcription.groovy
@@ -0,0 +1,27 @@
+package au.org.ala.volunteer
+
+class Transcription implements Serializable {
+
+    String fullyTranscribedBy
+    Date dateFullyTranscribed
+    String fullyTranscribedIpAddress
+    UUID transcribedUUID // unique id for the transcription
+    String fullyValidatedBy
+    Date dateFullyValidated
+
+    static belongsTo = [task:Task, project: Project]
+
+    static constraints = {
+        task nullable: false
+        fullyTranscribedBy nullable: true
+        dateFullyTranscribed nullable: true
+        fullyTranscribedIpAddress nullable: true
+        transcribedUUID nullable: true
+    }
+
+    static mapping = {
+         task lazy: false
+         transcribedUUID type: 'pg-uuid'
+    }
+
+}
