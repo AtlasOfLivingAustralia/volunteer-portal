1bc4e0125817503baf00c9ca2b02c8306f357b3c
Chris <chris.godwin.ala@gmail.com>
2019-07-29 16:10:13 +1000

    Tests for the ExportService #351 #352 #353

--- src/test/groovy/au/org/ala/volunteer/ExportServiceSpec.groovy
+++ src/test/groovy/au/org/ala/volunteer/ExportServiceSpec.groovy
@@ -24,27 +24,27 @@ class ExportServiceSpec extends Specification {
         response = new GrailsMockHttpServletResponse()
 
         setupData()
-
-//        mockDomain(Field, [field])
-//        mockDomain(Task, [task])
-//        mockDomain(Project, [project])
     }
 
     private void setupData() {
 
         List fieldNames = ['scientificName', 'occurenceRemarks']
 
+        // This is necessary as the export uses the domain object ids and they can't be assigned
+        // without the mocking framework.
         Template template = new Template(viewParams:[exportGroupByIndex:true])
         project = new Project(tasks:new HashSet(), name:"test project", template: template)
-        Task task = new Task(id:1, transcriptions: new HashSet())
-        Transcription transcription = new Transcription(id:1, task:task)
+        mockDomain(Project, [project])
+
+        Task task = new Task(id:1, transcriptions: new HashSet(), project:project)
+        mockDomain(Task, [task])
+        Transcription transcription = new Transcription(id:2, task:task, project:project)
+        mockDomain(Transcription, [transcription])
         task.transcriptions.add(transcription)
 
         fieldNames.each {
-            fields << new Field(task:task, transcription: transcription, name:it, value:it+'_value')
+            fields << new Field(task:task, transcription: transcription, name:it, value:it+'_value', recordIdx: 0)
         }
-
-        task.project = project
         project.tasks.add(task)
 
     }
@@ -60,11 +60,11 @@ class ExportServiceSpec extends Specification {
 
         then:
         1 * fieldService.getAllFieldsWithTasks(taskList) >> fields
-        1 * fieldService.getMaxRecordIndexByFieldForProject(project) >> [['occurenceRemarks', 1]]
+        1 * fieldService.getMaxRecordIndexByFieldForProject(project) >> [['occurenceRemarks', 0]]
         1 * taskService.getUserMapFromTaskList(taskList) >> [:]
 
-        //and:
-        //results.size() == 1 // One row, not counting headers
-        //results[0]['occurenceRemarks'] == 'value'
+        and:
+        results.size() == 1 // One row, not counting headers
+        results[0]['occurenceRemarks'] == 'occurenceRemarks_value'
     }
 }
