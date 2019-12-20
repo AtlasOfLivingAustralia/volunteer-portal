eef17304ecd8ba90faeb7db540766d396994e207
Simon Bear <simon.bear@csiro.au>
2019-12-20 16:26:01 +1100

    Fix project forum checkbox not working #284

--- grails-app/controllers/au/org/ala/volunteer/ForumController.groovy
+++ grails-app/controllers/au/org/ala/volunteer/ForumController.groovy
@@ -609,26 +609,14 @@ class ForumController {
         
         if (user && projectInstance && params.containsKey("watch")) {
             def watch = params.boolean("watch")
-            
-            def watchList = ProjectForumWatchList.findByProject(projectInstance)
-            if (!watchList) {
-                watchList = new ProjectForumWatchList(project: projectInstance)
-                watchList.save(failOnError: true)
-            }
 
+            forumService.watchProject(user, projectInstance, watch)
             if (watch) {
-                if (!watchList.containsUser(user)) {
-                    watchList.addToUsers(user)
-                }
                 results.message = "You will be sent a notification email when messages are posted to this project"
             } else {
-                if (watchList.containsUser(user)) {
-                    watchList.removeFromUsers(user)
-                }
                 results.message = "You will no longer be sent notification emails when messages are posted to this project"
             }
 
-            watchList.save()
             results.success = true;
         }
         
