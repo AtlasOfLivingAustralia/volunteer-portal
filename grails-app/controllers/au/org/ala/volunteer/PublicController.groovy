df0eec311ded2db8db18f655382e4e07a16d668f
Chris Dunstall <chris.dunstall@austmus.gov.au>
2021-03-17 14:34:51 +1100

    #435 Revision PublicController

--- grails-app/controllers/au/org/ala/volunteer/PublicController.groovy
+++ grails-app/controllers/au/org/ala/volunteer/PublicController.groovy
@@ -2,16 +2,13 @@ package au.org.ala.volunteer
 
 class PublicController {
 
-    def logService
-
-    def index() { }
 
     /**
-     * Do logouts through this app so we can invalidate the session.
-     *
-     * @param casUrl the url for logging out of cas
-     * @param appUrl the url to redirect back to after the logout
-     */
+     * Do logouts through this app so we can invalidate the session.
+     *
+     * @param casUrl the url for logging out of cas
+     * @param appUrl the url to redirect back to after the logout
+     */
     def logout() {
         log.info "Invalidating Session (PublicController.logout): ${session.id}"
         session.invalidate()
