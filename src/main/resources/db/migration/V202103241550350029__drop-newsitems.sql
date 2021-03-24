6245a943eb92cd0e082780a284852359422aa82d
Chris Dunstall <chris.dunstall@austmus.gov.au>
2021-03-24 16:29:31 +1100

    #444 Jooq table drop for News Item (and Domain removal)

--- src/main/resources/db/migration/V202103241550350029__drop-newsitems.sql
+++ src/main/resources/db/migration/V202103241550350029__drop-newsitems.sql
@@ -3,4 +3,4 @@ Add as-idempotent-as-possible DDL statements here
 See: http://www.jeremyjarrell.com/using-flyway-db-with-distributed-version-control/
 */
 
-DROP TABLE IF EXISTS project_association;
+DROP TABLE IF EXISTS news_item;
