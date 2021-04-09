a75e618d8628beb73c7b840d77d94773e952b290
Chris Dunstall <chris.dunstall@austmus.gov.au>
2021-04-09 16:45:42 +1000

    #435 Revision CollectionEventController
    Removed deprecated code.

--- grails-app/controllers/au/org/ala/volunteer/CollectionEventController.groovy
+++ grails-app/controllers/au/org/ala/volunteer/CollectionEventController.groovy
@@ -1,12 +1,10 @@
 package au.org.ala.volunteer
 
 import grails.converters.JSON
-import org.springframework.web.multipart.MultipartFile
 
 class CollectionEventController {
 
     def collectionEventService
-    def userService
 
     def searchFragment() {
         def taskInstance = Task.get(params.int("taskId"))
@@ -22,22 +20,22 @@ class CollectionEventController {
 
         def taskInstance = Task.get(params.int("taskId"))
 
-        int maxRows = params.maxResults ? params.int("maxResults") : 500;
+        int maxRows = params.maxResults ? params.int("maxResults") : 500
 
         def eventDate = params.eventDate
         def events = []
-        def finished = false;
+        def finished = false
 
         def collectors = []
         (0..3).each {
             collectors << params["collector" + it] ?: ""
         }
 
-        int loopcount = 0;
+        int loopcount = 0
 
         def locality = params.search_locality
 
-        def queryDate = eventDate.toString();
+        def queryDate = eventDate.toString()
 
         def useExpandedSearch = params.boolean('expandedSearch')
 
@@ -56,61 +54,36 @@ class CollectionEventController {
                 events = collectionEventService.findCollectionEvents(collectionCode, queryCollectors, queryDate, locality, maxRows)
 
                 if (events && events.size() > 0 || !useExpandedSearch) {
-                    finished = true;
-                    break;
+                    finished = true
+                    break
                 }
 
                 collectorNames.each { if (it.size() > 0) {
-                    it.remove(0);
+                    it.remove(0)
                 }}
 
             }
             if (queryDate.indexOf("-") > 0 && useExpandedSearch) {
                 queryDate = queryDate.substring(0, queryDate.lastIndexOf("-"))
             } else {
-                finished = true;
-                break;
+                finished = true
+                break
             }
 
-            loopcount++;
+            loopcount++
         }
 
 
         [collectors:collectors, eventDate: eventDate, collectionEvents: events, searchWidened: loopcount > 1, taskInstance: taskInstance]
     }
 
-    def load() {
-        if (userService.isAdmin()) {
-            def collectionCodes = collectionEventService.getCollectionCodes()?.join(", ");
-
-            render(view: 'load', model: [collectionCodes: collectionCodes])
-        } else {
-            redirect(uri: "/")
-        }
-    }
-
-    def loadCSV() {
-        if (userService.isAdmin()) {
-            def collectionCode = params.collectionCode;
-            MultipartFile f = request.getFile('csvfile')
-
-            def results = collectionEventService.importEvents(collectionCode, f)
-
-            flash.message = results.message
-
-            render(view: 'load')
-        } else {
-            redirect(uri: "/")
-        }
-    }
-
     def getCollectionEventJSON() {
 
-        CollectionEvent event = null;
+        CollectionEvent event = null
         if (params.collectionEventId) {
             event = CollectionEvent.get(params.long("collectionEventId"))
         } else if (params.externalCollectionEventId && params.institutionCode) {
-            event = CollectionEvent.findByExternalEventIdAndInstitutionCode(params.long('externalCollectionEventId'), params.institutionCode);
+            event = CollectionEvent.findByExternalEventIdAndInstitutionCode(params.long('externalCollectionEventId'), params.institutionCode)
         }
 
         if (event) {
