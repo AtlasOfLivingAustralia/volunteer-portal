3b2370e4759a131d9a7e5b3c8b008454d4ce6a80
Chris Dunstall <chris.dunstall@austmus.gov.au>
2021-07-15 13:58:20 +1000

    #439 Relocated service code from FrontPageService to ProjectService to fix issue with batch db updating creating an exception

--- grails-app/services/au/org/ala/volunteer/ProjectService.groovy
+++ grails-app/services/au/org/ala/volunteer/ProjectService.groovy
@@ -4,6 +4,8 @@ import com.google.common.base.Stopwatch
 import grails.transaction.Transactional
 import grails.web.mapping.LinkGenerator
 import grails.web.servlet.mvc.GrailsParameterMap
+import groovy.sql.Sql
+import groovy.time.TimeCategory
 import groovy.transform.Immutable
 import groovyx.gpars.actor.Actors
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
@@ -19,6 +21,8 @@ import org.springframework.context.i18n.LocaleContextHolder
 
 import javax.annotation.PreDestroy
 import javax.imageio.ImageIO
+import javax.sql.DataSource
+import java.util.concurrent.ThreadLocalRandom
 
 import static au.org.ala.volunteer.jooq.tables.ForumMessage.FORUM_MESSAGE
 import static au.org.ala.volunteer.jooq.tables.ForumTopic.FORUM_TOPIC
@@ -57,6 +61,7 @@ class ProjectService {
     private static final Condition getACTIVE_ONLY() { PROJECT.INACTIVE.eq(false) | PROJECT.INACTIVE.isNull() }
 
     def userService
+    DataSource dataSource
     LinkGenerator grailsLinkGenerator
     def projectTypeService
     def forumService
@@ -870,6 +875,120 @@ class ProjectService {
             return result[0]
         } else {
             return 0
+        }
+    }
+
+    /**
+     * Randomly select a project from active, non-archived projects. Only selects from projects that have not been
+     * perviously selected in the last 5 days (to prevent frequent reselection) and is less than 90% completed.
+     *
+     * @return the randomly selected project. If no active, unarchived projects exist, return null.
+     */
+    def selectRandomProject() {
+        def startDate = null
+        def endDate = new Date()
+        use(TimeCategory) {
+            startDate = endDate - 5.days
+        }
+        if (!startDate) startDate = new Date()
+        log.debug("Random project selection time frame: ${startDate}")
+
+        def query = """\
+            select p.id,
+                   sum(case when ta.is_fully_transcribed = true then 1 else 0 end) as transcribed, 
+                   sum(case when ta.fully_validated_by is not null then 1 else 0 end) as validated,
+                   count(ta.id) as total_tasks,
+                   (100.0*(cast(sum(case when ta.is_fully_transcribed = true then 1 else 0 end) as decimal(7,2)) / count(ta.id))) as completion
+              from project p
+              join task ta on (ta.project_id = p.id)
+            where p.archived = false
+              and p.inactive = false
+              and (p.potd_last_selected <= :startDate or p.potd_last_selected is null)
+            group by p.id, name
+            having sum(case when ta.fully_validated_by is not null then 1 else 0 end) < count(ta.id) """.stripIndent()
+
+        def sql = new Sql(dataSource)
+        def projectList = []
+        def backupList = []
+        def results = sql.rows(query, [startDate: startDate.toTimestamp()])
+
+        if (results.size() > 0) {
+            results.each { row ->
+                Project project = Project.get(row.id as long)
+                if (project && row.completion < 100.0) projectList.add(project)
+                else if (project) backupList.add(project)
+            }
+        }
+
+        log.debug("Project Lists to choose from: projectList: ${projectList.size()}")
+        log.debug("Project Lists to choose from: backupList: ${backupList.size()}")
+
+        // Backup checking. If there are no projects that are open and yet to be transcribed, use the
+        // backup list (projects needing validation). If that list is empty, just grab the list of projects.
+        if (projectList.size() == 0 && backupList.size() > 0) {
+            log.debug("Project list is empty, going to backupList")
+            projectList = backupList
+        } else if (projectList.size() == 0 && backupList.size() == 0) {
+            log.debug("No projects to choose from, getting any project")
+            projectList = Project.createCriteria().list {
+                and {
+                    eq('archived', false)
+                    eq('inactive', false)
+                    or {
+                        lt('potdLastSelected', startDate)
+                        isNull('potdLastSelected')
+                    }
+                }
+            } as List
+            log.debug("Projects list: ${projectList.size()}")
+        }
+
+        // Randomly select a project from the list.
+        int randomIndex = ThreadLocalRandom.current().nextInt(0, projectList.size() + 1);
+        Project projectToDisplay = projectList.get(randomIndex) as Project
+        log.debug("Project selected: ${projectToDisplay}")
+        projectToDisplay.potdLastSelected = new Date()
+        projectToDisplay.save(failOnError: true, flush: true)
+
+        return projectToDisplay
+    }
+
+    /**
+     * Determines if the difference between the current datetime is a time that allows for a new random project to be
+     * selected.
+     * @param randomProjectDateUpdated the last datetime the random project was updated.
+     * @return
+     */
+    def isTimeToUpdateRandomProject(Date randomProjectDateUpdated) {
+        def currentDate = new Date().getAt(Calendar.DAY_OF_YEAR)
+        if (!randomProjectDateUpdated) return true
+        def lastDate = randomProjectDateUpdated.getAt(Calendar.DAY_OF_YEAR)
+        // If we've gone over to the next year, return true.
+        if (currentDate < lastDate) return true
+        return currentDate - lastDate >= 1
+    }
+
+    /**
+     * Intended to be called from index page, checks if time to update the project of the day. If so, gets a new
+     * project and updates the frontpage parameter.
+     * @param frontPage The front page details.
+     */
+    def checkProjectOfTheDay(FrontPage frontPage) {
+        log.debug("Checking if it's time to change the PotD")
+        if (isTimeToUpdateRandomProject(frontPage.randomProjectDateUpdated) ||
+                isProjectComplete(frontPage.projectOfTheDay)) {
+            log.debug("Yes, updating PotD...")
+            def project = selectRandomProject()
+            if (project) {
+                log.debug("New PotD: ${project}")
+                frontPage.projectOfTheDay = project
+                frontPage.randomProjectDateUpdated = new Date()
+                frontPage.save(failOnError: true, flush: true)
+            }
+            return frontPage.projectOfTheDay
+        } else {
+            log.debug("Using existing PotD: ${frontPage.projectOfTheDay}")
+            return frontPage.projectOfTheDay
         }
     }
 }
