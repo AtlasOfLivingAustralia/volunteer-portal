07098ab5cd51635776864466bcf6c3fae99ef4dd
Chris Dunstall <chris.dunstall@australian.museum>
2022-11-18 17:11:34 +1100

    #513 Initial changes for grails 4 step of upgrade
    build.gradle.backup.txt is the grails 3.3 file, can be removed once upgrade is completed.

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -108,11 +108,4 @@ grails {
             }
         }
     }
-//    gorm {
-//        'default' {
-//            mapping = {
-//                id generator: 'org.hibernate.id.enhanced.SequenceStyleGenerator', params: [prefer_sequence_per_entity: true]
-//            }
-//        }
-//    }
 }
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -42,7 +42,8 @@ beans = {
             baselineOnMigrate = application.config.flyway.baselineOnMigrate
             def outOfOrderProp = application.config.getProperty('flyway.outOfOrder', Boolean, false)
             outOfOrder = outOfOrderProp
-            locations = application.config.flyway.locations ?: 'classpath:db/migration'
+            //locations = application.config.flyway.locations ?: 'classpath:db/migration'
+            locations = ['classpath:db/migration']
             if (application.config.flyway.baselineVersion) baselineVersionAsString = application.config.flyway.baselineVersion.toString()
         }
 
