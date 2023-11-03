40bf270357ec62ea3db6d80967c2b605a47d7ea0
Chris Dunstall <chris.dunstall@austmus.gov.au>
2023-11-03 12:02:34 +1100

    DG-10 Updated script to fix server startup errors

--- src/main/resources/db/migration/V202310301306580315__user_welcome.sql
+++ src/main/resources/db/migration/V202310301306580315__user_welcome.sql
@@ -3,9 +3,8 @@
     By: Chris Dunstall
 */
 
-ALTER TABLE "vp_user"
-    ADD "welcome_date" TIMESTAMP NULL;
-COMMENT ON COLUMN "vp_user"."welcome_date" IS '';
+ALTER TABLE vp_user ADD COLUMN IF NOT EXISTS welcome_date TIMESTAMP NULL;
+COMMENT ON COLUMN vp_user.welcome_date IS '';
 
 -- Set field to true for all pre-existing users.
 UPDATE vp_user SET welcome_date = '2023-10-01 00:00:00' WHERE welcome_date IS NULL;
