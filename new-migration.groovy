480832bf90e1e5c0367fdb3f7526f3385214dcd9
Chris Dunstall <chris.dunstall@austmus.gov.au>
2025-04-11 17:08:38 +1000

    Updated new db migration script template

--- new-migration.groovy
+++ new-migration.groovy
@@ -4,14 +4,17 @@ import java.text.*
 def date = new Date()
 def df = new SimpleDateFormat('yyyyMMddHHmmssSSSS')
 def ts = df.format(date)
-def name = args.length > 0 ? args[0] : "RENAME_ME"
+def name = args.length > 0 ? args[0].replace(" ", "-") : "RENAME_ME"
 def file = new File(new File(sourceURI).parentFile,"src/main/resources/db/migration/V${ts}__${name}.sql")
 if (file.createNewFile()) {
   file.withWriter { fw ->
     fw.write('/*\n')
-    fw.write('Add as-idempotent-as-possible DDL statements here\n')
-    fw.write('See: http://www.jeremyjarrell.com/using-flyway-db-with-distributed-version-control/\n')
+    fw.write('    Author: Your name\n')
+    fw.write('\n')
+    fw.write('    Add as-idempotent-as-possible DDL statements here\n')
+    fw.write('    See: http://www.jeremyjarrell.com/using-flyway-db-with-distributed-version-control/\n')
     fw.write('*/')
+    fw.write('\n')
   }
   println "Created $file"
   return 0
