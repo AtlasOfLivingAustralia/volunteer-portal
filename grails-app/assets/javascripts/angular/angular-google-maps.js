72656151a7df4e651b51c60cc0ec5efbc4057ea0
Chris Dunstall <chris.dunstall@austmus.gov.au>
2026-09-15 12:37:14 +1000

    DG-255 Decoupling/removal of old Wizard, qTip, angular code

--- grails-app/assets/javascripts/angular/angular-google-maps.js
+++
@@ -1,5 +0,0 @@
-//= encoding UTF-8
-//  assume angular-assets
-//= require underscore
-//= require angular-simple-logger
-//= require compile/angular-google-maps/2.2.1/angular-google-maps.min.js
--- grails-app/assets/javascripts/angular/angular-qtip.js
+++
@@ -1,4 +0,0 @@
-//= encoding UTF-8
-//= require angular-assets
-//= require qtip
-//= require compile/angular-qtip/angular-qtip.js
